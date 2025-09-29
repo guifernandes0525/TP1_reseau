d927628abefeb2f78b8a28d4b380455f7101c6b9
guifernandes0525 <guifernandes0525@gmail.com>
2025-09-29 12:12:53 +0200

    serveur pret 2

--- src/server.c
+++ src/server.c
@@ -6,10 +6,78 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include "calculator.h"
+#include <stdbool.h> 
 
 #define INIT_PORT 22000
 #define BUFF_LEN 1024
 #define MAX_CLIENT 10
+
+typedef struct expression {
+    float op1, op2;
+    char operator;
+    int quit;
+    
+} EXPRESSION;
+
+typedef struct result {
+    float result;
+    char * message[MSG_LEN];
+    
+} RESULT;
+
+
+void calculate(EXPRESSION * exp, RESULT * res)  {
+
+    switch (exp->operator) {
+    case '+':
+        res->result = exp->op1 + exp->op2;
+        strcpy(res->message,"Sum is: ");
+        break;
+
+    case '-':
+        res->result = exp->op1 - exp->op2;
+        strcpy(res->message,"Subtraction is: ");
+        break;
+
+    case '*':
+        res->result = exp->op1 * exp->op2;
+        strcpy(res->message,"Multiplication is: ");
+        break;
+
+    case '/':
+        if (exp->op2 == 0) { // division by zero
+            res->result = 0;
+            strcpy(res->message,"KABOOM!");
+        }
+        else {
+            res->result = exp->op1 / exp->op2;
+            strcpy(res->message,"Division is: ");
+        }
+        break;
+
+    default:
+        res->result = 0;
+        strcpy(res->message,"Calculator Error");
+        break;
+    
+    }
+}
+
+bool valid_range(EXPRESSION * exp) {
+    if (exp->op1 < 0 || exp->op1 > 10000 || exp->op2 < 0 || exp->op2 > 10000)
+        return true;
+    else
+        return false;
+}
+
+bool quit(char *input_str) {
+    to_lower(input_str);
+    
+    if (strcmp(input_str, "quit") == 0) // input is "quit"
+        return true;
+    else
+        return false;
+};
 
 int main(int argc, char *argv[]) {
 
